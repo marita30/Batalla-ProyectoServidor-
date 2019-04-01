52cf50dad292f0597e3118671c0e0e8b1a65383a
M.J <marita.jose.ch@gmail.com>
2019-04-01 23:50:29 +0200

    Agregando otra prueba llamada game_over con su clase , vista y controlador (errores en la terminal por capybara---- ejercicio LOsing and Winning

--- app.rb
+++ app.rb
@@ -21,11 +21,23 @@ class Battle < Sinatra::Base
     erb :play
   end
 
+  post '/attack' do
+    Attack.run($game.opponent_of($game.current_turn))
+    if $game.game_over?
+      redirect '/game-over'
+    else
+      redirect '/attack'
+    end
+  end
+
   get '/attack' do
    @game = $game
-   @game.attack(@game.player_2)
-   @game.switch_turns
     erb :attack
+  end
+
+  get '/game-over' do
+    @game = @game
+    erb :game_over
   end
 
   # start the server if ruby file executed directly
--- lib/game.rb
+++ lib/game.rb
@@ -14,17 +14,36 @@ class Game
 		@players.last
 	end
 
-	def attack(player)
-		player.receive_damage
-	end 
-
 	def switch_turns
 	 @current_turn = opponent_of(current_turn)
 	end
 
-	private
-
 	def opponent_of(the_player)
 	 @players.select { |player| player != the_player }.first
+	end
+
+	def game_over?
+		losing_players.any?
+	end
+
+	def loser
+		losing_players.first
+		
+	end
+
+	def attack(player)
+		player.receive_damage
+	end 
+
+
+	private
+	attr_reader :players
+
+	def losing_players
+		players.select { |player| player.hit_points <= 0 }
+	end
+
+	def players_who_are_not(the_player)
+		players.select { |player| player != the_player }
 	end
 end
