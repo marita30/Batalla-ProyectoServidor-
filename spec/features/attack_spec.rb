dde1d980bfbeb129cc545e16ad35e330d73ac5d9
M.J <marita.jose.ch@gmail.com>
2019-04-01 22:11:36 +0200

    Modificando los archivos para hacer la otra historia de usuario de ir disminuyendo los puntos --- ejercicio Implementing Hit Points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,12 @@ feature 'Attacking' do
     click_link 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
   end
+
+  scenario 'reduce player 2 HP by 10' do
+  	sign_in_and_play
+  	click_button 'Attack'
+  	click_link 'OK'
+  	expect(page).not_to have_content 'Mittens: 60HP'
+  	expect(page).to have_content 'Mittens: 5OHP'
+  end
 end
